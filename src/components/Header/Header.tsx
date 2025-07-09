f1201f86342d56688491735701e5be9cfbce1180
odilonskt <odilon123c@gmail.com>
2025-07-08 22:57:31 -0300

    🐛 fix: subtitled route name

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import '../../components/globais.css'
+import '../../app/globals.css'
 import { MdMenu } from "react-icons/md";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { usePathname } from "next/navigation";
@@ -10,11 +11,11 @@ import { useEffect, useState } from "react";
 
 
 
-export  const Header = ()=>{
+export default function Header (){
 
 const currentPath = usePathname();
 const[drawer,setDrawer] = useState(false);
-const[title,setTitle] = useState("ssssss");
+const[title,setTitle] = useState("futebol");
 
 useEffect(()=>{
     setTitle(document.title);
@@ -22,7 +23,7 @@ useEffect(()=>{
 },[currentPath]);
 
 return(
-<nav  className="flex items-center gap-6 justify-start md:justify-center bg-teal-600 py-2 sm:py-4 px-6" >
+<nav  className="flex items-center gap-6 justify-start md:justify-center -color-background py-2 sm:py-4 px-6 bg-sky-800" >
 
     <button className="sm:hidden" aria-label="Abrir menu" onClick={()=> setDrawer(true)}>
         <MdMenu size={24}/>
@@ -33,13 +34,10 @@ return(
     <Link href='/' className="border-2 rounded-md py-2 px-1 font-bold">Futebol</Link>
   </li>
   <li className="hidden sm:block">
-    <Link href='/' data-active={currentPath === '/'} className="data-[active=true]:underline">Cálculos</Link>
+    <Link href='/times' data-active={currentPath === '/times'} className="data-[active=true]:underline">Times</Link>
   </li>
   <li className="hidden sm:block">
-    <Link href='/sair' data-active={currentPath === '/sair'} className="data-[active=true]:underline">Times</Link>
-  </li>
-  <li className="hidden sm:block">
-    <Link href='/brasil' data-active={currentPath === '/brasil'} className="data-[active=true]:underline">Jogadores</Link>
+    <Link href='/jogadores' data-active={currentPath === '/jogadores'} className="data-[active=true]:underline">Jogadores</Link>
         </li>
         <li className="hidden sm:block">
     <Link href='https://github.com/odilonskt/M5-mineProjeto' className="flex gap-1 items-center" target="_blank">Github
@@ -56,14 +54,12 @@ tabIndex={drawer ? undefined: -1}
 onClick={()=>setDrawer(false)}
 className="bg-gradient-to-r from-background fixed top-0 left-0 bottom-0 right-0 transition-transform data-[open=false]:-translate-x-full">
 <ul className=" flex gap-4 flex-col p-4 w-60 h-full bg-background " onClick={e => e.stopPropagation()}>
-        <li className="border-b-2 border-white " >
-    <Link href='/' data-active={currentPath === '/'} className="data-[active=true]:underline">Cálculos</Link>
-        </li>
+
         <li className="border-b-2 border-white  ">
-    <Link href='/sair' data-active={currentPath === '/sair'} className="data-[active=true]:underline">Times</Link>
+    <Link href='/times' data-active={currentPath === '/times'} className="data-[active=true]:underline">Times</Link>
         </li>
         <li className="border-b-2 border-white ">
-    <Link href='/brasil' data-active={currentPath === '/brasil'} className="data-[active=true]:underline">Jogadores</Link>
+  <Link href='/jogadores' data-active={currentPath === '/jogadores'} className="data-[active=true]:underline">Jogadores</Link>
         </li>
         <li className="">
     <Link href='https://github.com/odilonskt/M5-mineProjeto' className="flex gap-1 items-center" target="_blank">Github
