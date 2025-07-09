eb051399784dd4ff8ec156625179f21ee52ace4d
odilonskt <odilon123c@gmail.com>
2025-07-08 23:34:33 -0300

    feat:  add  icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+            <head>
+        <link rel="icon" type="image/svg+xml" href="/bola_anos90.svg" />
+      
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
